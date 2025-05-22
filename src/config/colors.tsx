7db5344f04ade5df9c089539387dc9e80641727f
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-22 20:58:24 -0300

    chore: created file to config root colors system

---
+++ src/config/colors.tsx
@@ -0,0 +1,15 @@
+// Primary colors
+export const primaryColor: string = "#C3073F";
+export const primaryColorLight: string = "#950740";
+export const primaryColorDark: string = "#1A1A1D";
+
+// Secondary colors
+export const secondaryColor: string = "#950740";
+export const secondaryColorLight: string = "#C3073F";
+export const secondaryColorDark: string = "#1A1A1D";
+
+// Warning colors
+export const successColor: string = "#00B300";
+export const errorColor: string = "#FF0000";
+export const warningColor: string = "#FFA500";
+export const infoColor: string = "#00BFFF";
