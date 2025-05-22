3f29182bbf8c164cd6881f11da932d700113d721
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-22 20:55:53 -0300

    feat: created component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,69 @@
+// Styled Components
+import { Nav, Title, ListLinks } from "./styled";
+
+// Redux
+import * as Actions from "../../store/modules/auth/actions";
+
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+
+// React Router
+import { useNavigate } from "react-router-dom";
+
+// Router Dom
+import { Link } from "react-router-dom";
+
+// Toastify
+import { toast } from "react-toastify";
+
+// Icons
+import { FaListOl, FaCircle, FaPowerOff } from "react-icons/fa";
+import { IoMdLogIn, IoMdCreate } from "react-icons/io";
+
+// Types
+interface RootState {
+  auth: {
+    isLoggedIn: boolean;
+  };
+}
+
+export default function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
+  const handleLogout = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+
+    toast.info("Você saiu do sistema.");
+    dispatch(Actions.ButtonLoginClickFailure());
+    navigate("/");
+  };
+  return (
+    <Nav>
+      <Title>AppSchool</Title>
+      <ListLinks>
+        <Link to="/" rel="noopener noreferrer">
+          <FaListOl size={18} color="#fff" />
+        </Link>
+
+        {isLoggedIn && (
+          <Link to="/register" rel="noopener noreferrer">
+            <IoMdCreate size={18} color="#fff" />
+          </Link>
+        )}
+        {isLoggedIn ? (
+          <Link to="/logout" onClick={handleLogout} rel="noopener noreferrer">
+            <FaPowerOff size={18} color="#fff" />
+          </Link>
+        ) : (
+          <Link to="/login" rel="noopener noreferrer">
+            <IoMdLogIn size={18} color="#fff" />
+          </Link>
+        )}
+
+        {isLoggedIn && <FaCircle size={14} color="#54b659" />}
+      </ListLinks>
+    </Nav>
+  );
+}
---
+++ src/components/Header/styled.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+// Global Colors
+import { primaryColor } from "../../config/colors";
+
+export const Nav = styled.nav`
+  background-color: ${primaryColor};
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    color: #fff;
+    margin: 0 1rem;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      color: #000;
+      transform: scale(1.1);
+    }
+  }
+`;
+
+export const ListLinks = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+`;
