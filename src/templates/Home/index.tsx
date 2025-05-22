813ff894ed062568c9b4100fbf44af092cdef69b
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-22 20:54:18 -0300

    feat: created template Home from main page app

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,9 @@
+// Styled Components
+import {} from "./styled";
+
+// Conatiner Global
+import { Container } from "../../styles/globals";
+
+export default function NotFound() {
+  return <Container>Home</Container>;
+}
