961ab793e10eb54839f36fdfadb14ecb5683df22
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-24 21:04:57 -0300

    fix: refactor componente Title

--- src/components/Title/styled.tsx
+++ src/components/Title/styled.tsx
@@ -3,8 +3,18 @@ import styled from "styled-components";
 // Global Styles
 import * as Colors from "../../config/colors";
 
-export const TitleStyled = styled.h1`
+export const TitleStyled = styled.h4`
   color: ${Colors.primaryColor};
   padding: 0.5rem 0;
   font-size: ${Colors.fontSize4xl};
+
+  text-align: center;
+
+  @media only screen and (max-width: 800px) {
+    font-size: ${Colors.fontSize2xl};
+  }
+
+  @media only screen and (max-width: 665px) {
+    font-size: ${Colors.fontSizeLg};
+  }
 `;
