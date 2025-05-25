a3384cde205bafed086b6e6de4728b903875920e
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-24 22:37:19 -0300

    feat: add new data in helper

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,4 +1,7 @@
 import { FaUser } from "react-icons/fa";
 
 // Navigation Links Data
-export const liValues = [{ id: 0, text: "Login", icon: FaUser, link: "login" }];
+export const liValues = [
+  { id: 0, text: "Login", icon: FaUser, link: "login", bg: true },
+  { id: 1, text: "Criar Conta", icon: FaUser, link: "register", bg: false },
+];
