44c02fca13cf4ee907c4296ed39c79dd4321b60f
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-24 23:45:26 -0300

    chore: created list pf prodtucts from helpers

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -5,3 +5,21 @@ export const liValues = [
   { id: 0, text: "Login", icon: FaUser, link: "login", bg: true },
   { id: 1, text: "Criar Conta", icon: FaUser, link: "register", bg: false },
 ];
+
+// Navigation Products
+export const liProducts = [
+  {
+    id: 0,
+    text: "Casas",
+    img: "caminho da imagem",
+    alt: "Imagem do Produto referente á casas.",
+    link: "reserves?type=casa",
+  },
+  {
+    id: 1,
+    text: "Apartamentos",
+    img: "caminho da imagem",
+    alt: "Imagem do Produto referente á apartamentos.",
+    link: "reserves?type=ap",
+  },
+];
