5896ae9e31fa395ef322348fe0dea12160b322ed
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-25 18:59:46 -0300

    fix: refactor component Title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,8 +3,14 @@ import { TitleStyled } from "./styled";
 
 export interface TitleProps {
   text: string;
+  color: string;
+  fontSize: string;
 }
 
-export default function Title({ text }: TitleProps) {
-  return <TitleStyled>{text}</TitleStyled>;
+export default function Title({ text, color, fontSize }: TitleProps) {
+  return (
+    <TitleStyled $fontSize={fontSize} $color={color}>
+      {text}
+    </TitleStyled>
+  );
 }
--- src/components/Title/styled.tsx
+++ src/components/Title/styled.tsx
@@ -3,18 +3,26 @@ import styled from "styled-components";
 // Global Styles
 import * as Colors from "../../config/colors";
 
-export const TitleStyled = styled.h4`
-  color: ${Colors.primaryColor};
-  padding: 0.5rem 0;
-  font-size: ${Colors.fontSize4xl};
+export interface TitleProps {
+  $color: string;
+  $fontSize: string;
+}
+
+export const TitleStyled = styled.h1<TitleProps>`
+  color: ${(props) => (props.$color ? props.$color : Colors.primaryColor)};
+
+  font-size: ${(props) =>
+    props.$fontSize ? props.$fontSize : Colors.fontSize4xl};
 
   text-align: center;
 
   @media only screen and (max-width: 800px) {
-    font-size: ${Colors.fontSize2xl};
+    font-size: ${(props) =>
+      props.$fontSize ? props.$fontSize : Colors.fontSize4xl};
   }
 
   @media only screen and (max-width: 665px) {
-    font-size: ${Colors.fontSizeLg};
+    font-size: ${(props) =>
+      props.$fontSize ? props.$fontSize : Colors.fontSize4xl};
   }
 `;
