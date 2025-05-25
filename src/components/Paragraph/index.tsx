32bb282f17a47b31a96c647de68c7859198af3ad
Renan Nascimento <atomsfrontend@gmail.com>
2025-05-24 21:05:41 -0300

    fix: refactor componente Paragraph

--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -1,11 +1,22 @@
 export interface ParagraphProps {
   children: React.ReactNode;
   bg?: boolean;
+  colors?: string;
+  textCenter?: boolean;
 }
 
 // Styles
 import { StyledParagraph } from "./styled";
 
-export default function Paragraph({ children, bg = false }: ParagraphProps) {
-  return <StyledParagraph $bg={bg}>{children}</StyledParagraph>;
+export default function Paragraph({
+  children,
+  bg = false,
+  textCenter = false,
+  colors,
+}: ParagraphProps) {
+  return (
+    <StyledParagraph $color={colors} $bg={bg} $textCenter={textCenter}>
+      {children}
+    </StyledParagraph>
+  );
 }
--- src/components/Paragraph/styled.tsx
+++ src/components/Paragraph/styled.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 // Colors
-import { textPrimary, primaryColorLight } from "../../config/colors";
+import { fontSizeSm, textPrimary } from "../../config/colors";
 
 interface StyledParagraphProps {
   $bg?: boolean;
+  $color?: string;
+  $textCenter?: boolean;
 }
 
 export const StyledParagraph = styled.p<StyledParagraphProps>`
-  color: ${({ $bg }) => ($bg ? textPrimary : primaryColorLight)};
+  color: ${({ $color }) => ($color ? $color : textPrimary)};
+  text-align: ${({ $textCenter }) => ($textCenter ? "center" : "")};
+
+  @media only screen and (max-width: 665px) {
+    font-size: ${fontSizeSm};
+  }
 `;
